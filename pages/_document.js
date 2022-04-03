d5ce4ae9310043c65997fe01bf1ecabe953340c0
Mohamed H. Mohamed <mohamedtwice@gmail.com>
2022-04-03 02:03:28 -0500

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
 
 export default class MyDocument extends Document {
   render() {
@@ -6,6 +7,19 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+            <script async src={gtag} />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `
+                }}
+            />
         </Head>
         
         <body>
